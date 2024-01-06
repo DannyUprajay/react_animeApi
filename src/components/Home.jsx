255b1f4663ddf57d8b319aa66e7aff76bfd6fe43
danny <danny.uprajay@gmail.com>
2024-01-06 17:43:45 +0100

    enfin fini la search bar

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,71 +1,60 @@
-import React,{useState, useEffect} from "react";
+import React, { useState, useEffect, useRef } from "react";
 import axios from "axios";
-import {useNavigate} from "react-router-dom";
-import MangaGenre from "./MangaGenre.jsx";
-
+import { useNavigate } from "react-router-dom";
 
 const Home = (props) => {
-let navigate = useNavigate()
+    let navigate = useNavigate();
 
-    const [url, setUrl] = useState('https://api.jikan.moe/v4/anime');
+    // const [url, setUrl] = useState("https://api.jikan.moe/v4/anime?q=naruto");
+    // const [url, setUrl] = useState("https://api.jikan.moe/v4/anime");
     const [animes, setAnime] = useState([]);
-    // const [selectedGenre, setselectedGenre] = useState();
     const [search, setSearch] = useState("");
 
-    const handleClick = (valeur) => {
-        console.log("Valeur cliquée :", valeur);
-
-    };
+    const inputRef = useRef(null);
 
     useEffect(() => {
-        const fetchData = async ()=>{
+        const fetchData = async () => {
             try {
-                const res = await axios.get(url);
+                const res = await axios.get(`https://api.jikan.moe/v4/anime?q=${search}`);
                 setAnime(res.data.data);
-                console.log(res.data.data)
+                console.log(res.data.data);
             } catch (error) {
-                console.error('Error fetching data:', error);
-
+                console.error("Error fetching data:", error);
             }
-        }
+        };
         fetchData();
-    }, []);
+    }, [search]);
 
+    const searchAnime = () => {
+        setSearch(inputRef.current.value);
+        console.log(inputRef.current.value);
+    };
 
-    const searchAnime=()=>{
-        setUrl(`https://api.jikan.moe/v4/anime${search}`)
-    }
     return (
         <>
+            <h1 className="text-3xl text-center font-bold ">
+                Liste des mangas
+            </h1>
+
+            <div>
+                <input type="text" ref={inputRef} placeholder="Search Marvel..." />
+                <button onClick={searchAnime}>Search</button>
+            </div>
+
+            <section>
 
 
-        <h1 className="text-3xl text-center font-bold ">
-          Liste des mangas
-        </h1>
+                {animes.map((anime,index) =>(
+                    <div key={index}>
+                    <h2>{anime.title}</h2>
+                        <img src={anime.images.jpg.image_url} alt=""/>
+                    </div>
+                ))}
 
-            <input type="text" placeholder="Search Marvel..." onChange={e=>setSearch(e.target.value)}
-                   onKeyPress={searchAnime}/>
 
-            <section className={'flex items-center'}>
-                <div className={'grid grid-cols-4 lg:grid-cols-4 gap-2 md:grid-cols-1 container mx-auto'}>
-
-                    {animes.map((anime, index) => (
-                        <div className={'card col'} key={anime.mal_id} onClick={() => navigate(`/${anime.mal_id}`)}>
-                            <img className={'cursor-pointer'} src={`${anime.images.jpg.image_url}`} alt="" />
-                            {anime.genres.map((genre, index) => (
-                                <div key={index}>
-                                    <h2>{genre.name}</h2>
-                                    <p>{genre.type}</p>
-                                </div>
-                            ))}
-                        </div>
-                    ))}
-                </div>
             </section>
-
-        {/*<MangaGenre/>*/}
         </>
-    )
-}
+    );
+};
 
 export default Home;
